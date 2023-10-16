b283c7831f558a334c7784d2a16ee70e548cfac9
ericaostini <ericaostini14@gmail.com>
2023-10-16 16:40:31 +0200

    add product to listaSpesa

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
-let product;
+let product = "";
+let listaSpesa = [];
 while (product !== "stop" ){
     product = prompt("Che prodotto vuoi acquistare?");
     console.log(product);
+    listaSpesa.push(product);
 }
+listaSpesa.pop(-1);
+console.log(listaSpesa);
